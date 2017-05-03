76b5c67d8fdd5d295fa6532e17519d2d5589ebfa
Lucio <V-FonsL-Ave@ae.com>
2017-05-02 21:14:47 -0300

    Initial commit

---
+++ GETestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GETestApp
+//
+//  Created by Avenue Brazil on 02/05/17.
+//  Copyright © 2017 Lucio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Pods/Target Support Files/Pods-GETestAppTests/Pods-GETestAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GETestAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_GETestAppTests
+@end
